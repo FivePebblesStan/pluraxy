1b955fb00f42bca2e92052ec109487cd05515f19
marethyu42 <155923809+marethyu42@users.noreply.github.com>
2025-12-03 11:48:14 -0800

    Added .gitignore

--- simply-plural-api-test.py
+++ simply-plural-api-test.py
@@ -1,3 +1,6 @@
+api_key = open("api-key.txt").read()
+
+# ==== 
 import requests
 
 url = "https://api.apparyllis.com/v1/user/analytics"
@@ -5,7 +8,7 @@ url = "https://api.apparyllis.com/v1/user/analytics"
 payload = {}
 headers = {
   'Accept': 'application/json',
-  'Authorization': '<Authorization>'
+  'Authorization': api_key
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
