2995c1d9a782a8b390d48e32cf37e114d11bd8eb
FivePebblesStan <125769461+FivePebblesStan@users.noreply.github.com>
2025-12-04 10:36:29 -0800

    Update discord-bot-test.py
    gets the first fronter only; test
    used to be first-fronter-discord-test.py

--- discord-bot-test.py
+++ discord-bot-test.py
@@ -1,13 +1,13 @@
 import discord
 import os
-import random
 import requests
 from discord.ext import commands
 from dotenv import load_dotenv
 from discord.ui import Modal, TextInput, View, Button
 
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+api_key = open("api-key.txt").read()
+token = open("discord-token.txt").read()
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -16,6 +16,24 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="px.", intents=intents)
 active_games = {}
+
+url1 = "https://api.apparyllis.com/v1/fronters/"
+
+payload = {}
+headers = {
+  'Authorization': api_key
+}
+
+response1 = requests.request("GET", url1, headers=headers, data=payload)
+
+sysID = response1.json()[0]["content"]["uid"]
+memID1 = response1.json()[0]["content"]["member"]
+
+url2 = "https://api.apparyllis.com/v1/member/" + sysID + "/" + memID1
+
+response2 = requests.request("GET", url2, headers=headers, data=payload)
+
+fronter = response2.json()["content"]["name"]
 
 @bot.event
 async def on_ready():
@@ -26,14 +44,13 @@ class testView(View):
         super().__init__()
         self.ctx = ctx
 
-    @discord.ui.button(label="wawawa", style=discord.ButtonStyle.green)
+    @discord.ui.button(label=":3", style=discord.ButtonStyle.green)
     async def wawawa(self, interaction: discord.Interaction, button: Button):
-        await interaction.response.send_message("wawawa", ephemeral=True)
+        await interaction.response.send_message(fronter + "says: wawawawawawawwawawa", ephemeral=True)
 
 @bot.command(name="test")
 async def test(ctx):
     view = testView(ctx)
-    await ctx.send("hi!", view=view)
+    await ctx.send("Current fronter is " + fronter + "!", view=view)
 
-token = open("discord-token.txt").read()
 bot.run(token)
