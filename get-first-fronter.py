a9cfb72af7407c7c07892e1564bf33520b22fd5a
marethyu42 <155923809+marethyu42@users.noreply.github.com>
2025-12-15 00:17:07 -0800

    Generalized into a function

--- get-first-fronter.py
+++ get-first-fronter.py
@@ -1,23 +1,22 @@
 import requests
 
-api_key = open("api-key.txt").read()
+api_key = open("api-key.txt").read().strip()
+def get_all_fronters(api_key):
+    url1 = "https://api.apparyllis.com/v1/fronters/"
+    payload = {}
+    headers = {'Authorization': api_key}
+    response1 = requests.request("GET", url1, headers=headers, data=payload)
+    print(response1.json())
+    sysID,memID = [],[]
+    for alter in response1.json():
+        sysID.append(alter["content"]["uid"])
+        memID.append(alter["content"]["member"])
+    fronters = []
+    for fronter in range(len(sysID)):
+        url2 = "https://api.apparyllis.com/v1/member/" + sysID[fronter] + "/" + memID[fronter]
+        response2 = requests.request("GET", url2, headers=headers, data=payload)
+        fronters.append(response2.json()["content"]["name"])
+    return fronters
 
-url1 = "https://api.apparyllis.com/v1/fronters/"
+print(get_all_fronters(api_key))
 
-payload = {}
-headers = {
-  'Authorization': api_key
-}
-
-response1 = requests.request("GET", url1, headers=headers, data=payload)
-
-sysID = response1.json()[0]["content"]["uid"]
-memID1 = response1.json()[0]["content"]["member"]
-
-url2 = "https://api.apparyllis.com/v1/member/" + sysID + "/" + memID1
-
-response2 = requests.request("GET", url2, headers=headers, data=payload)
-
-fronter = response2.json()["content"]["name"]
-
-print(fronter)
