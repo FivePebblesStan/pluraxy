ad9aba059b329c2e8e9cb80a3936dd4726e84ace
FivePebblesStan <125769461+FivePebblesStan@users.noreply.github.com>
2026-01-07 11:48:10 -0800

    Update setup.py
    Basically all of px.setup!
    Just need to add button disabling, and for the buckets issue to be fixed (selecting a bucket only works if profile's name is typed exactly)

--- setup.py
+++ setup.py
@@ -82,11 +82,12 @@ class startModal(Modal):
             await interaction.response.send_message("Something went wrong. Please make sure your SimplyPlural token is correct and try again.", ephemeral=True)
         else:
             await interaction.response.send_message("Your SimplyPlural token is valid. Hello, " + response.json()["content"]["username"] +"!", view=nextView(self.ctx), ephemeral=True)
+        user = await self.ctx.bot.fetch_user(userId)
         with open("pluraxy-users.txt", "a") as file:
-            file.write(spToken + ", " + response.json()["content"]["username"] + ", " + userId + ", " + client.get_user(self, userId))
+            file.write(spToken + ", " + response.json()["content"]["username"] + ", " + str(userId) + ", " + str(user))
 
 
-#view for interstep of px.setup
+#view for interstep1.5 of px.setup
 class nextView(View):
     def __init__(self, ctx):
         super().__init__()
@@ -96,7 +97,7 @@ class nextView(View):
     async def eepy(self, interaction: discord.Interaction, button: Button):
         await interaction.response.send_message("The way Pluraxy knows which alters in your are messageable is via looking at the buckets of a SimplyPlural profile you choose, and allowing messaging to all the profiles in those buckets.\n"
                                                 "E.g. profile 1 is in buckets A and B, profile 2 is in buckets B and C, and profile 3 is in buckets C and D. If profile 1 is chosen as the bucket basis, only profiles 1 and 2 will be messageable, not profile 3.\n"
-                                                "So, please press this button and type in the name of a profile on your SimplyPlural that is in all the buckets you would like to be accessible. (You may create an extra profile for this purpose if necessary.)", view=bucketsView(self.ctx), ephemeral=True)
+                                                "So, please press this button and type in the name of a profile on your SimplyPlural that is in all the buckets you would like to be accessible. (You may create a temporary extra profile for this purpose if necessary.)", view=bucketsView(self.ctx), ephemeral=True)
 
 # view for pt2 of px.setup
 class bucketsView(View):
@@ -120,9 +121,38 @@ class bucketsModal(Modal):
         profile = self.input.value
         global buckets
         buckets = choose_bucket(spToken, profile)
-        await interaction.response.send_message("The selected bucket(s) is/are: " + buckets[0], ephemeral=True)
+        await interaction.response.send_message("The selected bucket(s) is/are: " + buckets + "\n"
+                                                "-# (If you created a temporary profile for this, you may now delete it)", view=nexttView(self.ctx), ephemeral=True)
         with open("pluraxy-users.txt", "a") as file:
             file.write(", " + buckets + "\n")
+
+#view for interstep2.5 of px.setup
+class nexttView(View):
+    def __init__(self, ctx):
+        super().__init__()
+        self.ctx = ctx
+
+    @discord.ui.button(label="Next", style=discord.ButtonStyle.green)
+    async def eepy(self, interaction: discord.Interaction, button: Button):
+        await interaction.response.send_message("Lastly, would you like it so that only users who are friends with you on SimplyPlural can message those in your system?", view=friendsView(self.ctx), ephemeral=True)
+
+#view for pt.3 of px.setup
+class friendsView(View):
+    def __init__(self, ctx):
+        super().__init__()
+        self.ctx = ctx
+
+    @discord.ui.button(label="Only SP friends can message", style=discord.ButtonStyle.grey)
+    async def eepiest(self, interaction: discord.Interaction, button: Button):
+        await interaction.response.send_message("Alright, only SP friends will be able to message.\n"
+                                                "Pluraxy setup is now complete, thank you! You can now use the bot.\n"
+                                                "Type px.message to send a message to someone!", ephemeral=True)
+
+    @discord.ui.button(label="Anyone can message", style=discord.ButtonStyle.grey)
+    async def eepier(self, interaction: discord.Interaction, button: Button):
+        await interaction.response.send_message("Alright, anyone will be able to message.\n"
+                                                "Pluraxy setup is now complete, thank you! You can now use the bot.\n"
+                                                "Type px.message to send a message to someone!", ephemeral=True)
 
 @bot.command(name="setup")
 async def setup(ctx):
@@ -130,7 +160,8 @@ async def setup(ctx):
     userId = ctx.author.id
     view = setupView(ctx)
     await ctx.send("Welcome to Pluraxy, a bot for messaging alters! Thank you for using this bot ^^\n"
-                   "Currently, Pluraxy only uses SimplyPlural in order to fetch system info. PluralKit may be added at some point as well."
+                   "Currently, Pluraxy only uses SimplyPlural in order to fetch system info. PluralKit may be added at some point as well.\n"
                    "To begin account setup using SimplyPlural, click the start button!", view=view)
 
 bot.run(token)
+#client.run(token)
